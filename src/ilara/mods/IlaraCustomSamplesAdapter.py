94202af10d1ae4e459a00ee18a07f7663ddf8550
Daniel Karanja <Daniel@cohesu.org>
2021-05-03 15:55:47 +0300

    Custom Adapter ***

--- src/ilara/mods/IlaraCustomSamplesAdapter.py
+++ src/ilara/mods/IlaraCustomSamplesAdapter.py
@@ -13,8 +13,35 @@ class IlaraCustomSamplesAdapter(object):
 
     def before_render(self):
         # Do your own stuff here. E.g., add search criteria in `contentFilter` variable
+                # Add a new filter status
+        draft_status = {
+            "id": "draft",
+            "title": "Draft",
+            "contentFilter": {
+                "review_state": "sample_draft",
+                "sort_on": "created",
+                "sort_order": "descending",
+            },
+            "columns": self.listing.columns.keys(),
+        }
+        self.listing.review_states.append(draft_status)
+
+        # Add the column
+        self.listing.columns["MyColumn"] = {
+            "title": "My column",
+            "sortable": False,
+            "toggle": True,
+        }
+
+        # Make the new column visible for all filter statuses
+        for filter in self.listing.review_states:
+            filter.update({"columns": self.listing.columns.keys()})
         return
 
     def folder_item(self, obj, item, index):
         # Do your own stuff here. E.g., set the value to display for a given column
+
+        sample = api.get_object(obj)
+        item["MyColumn"] = obj.getField("MyCustomAttribute").get(sample) or "Empty value"
+        
         return item
