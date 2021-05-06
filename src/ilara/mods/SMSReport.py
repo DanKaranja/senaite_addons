6518891457926cfeea0466f243c5d91ef4a0f4ec
Daniel Karanja <Daniel@cohesu.org>
2021-05-06 22:53:19 +0300

    Push API Test  ***

---
+++ src/ilara/mods/SMSReport.py
@@ -0,0 +1,58 @@
+from bika.lims import api
+from bika.lims.api import mail as mailapi
+from senaite.jsonapi.interfaces import IPushConsumer
+from zope import interface
+
+
+class SMSReport(object):
+    """Custom adapter for sending sms pdf reports to contacts
+    """
+    interface.implements(IPushConsumer)
+
+    def __init__(self, data):
+        self.data = data
+
+    def process(self):
+        """Send notifications to contacts
+        """
+        # Get query parameters
+        sample = data.get("sample")
+        phone_number = data.get("phone_number")
+
+        # Get pdf link
+        # emails = self.phone_number()
+
+        # Send the emails
+        # success = map(lambda e: self.send(e, subject, message), emails)
+        return self.get_Pdf(sample)
+
+    def get_Pdf(self,sample):
+        """Returns the pdf file of the sample report
+        """
+        query = {"portal_type": "AnalysisRequest","title":sample}
+        AnalysisRequests = map(api.get_object, api.search(query, "portal_catalog"))
+
+
+
+        # query = {"portal_type": ["Contact", "LabContact"]}
+        # contacts = map(api.get_object, api.search(query, "portal_catalog"))
+        # emails = map(lambda c: c.getEmailAddress(), contacts)
+        # emails = filter(None, emails)
+        return len(AnalysisRequests)
+
+    # def get_emails(self):
+    #     """Returns the emails from all registered contacts
+    #     """
+    #     query = {"portal_type": ["Contact", "LabContact"]}
+    #     contacts = map(api.get_object, api.search(query, "portal_catalog"))
+    #     emails = map(lambda c: c.getEmailAddress(), contacts)
+    #     emails = filter(None, emails)
+    #     return list(OrderedDict.fromkeys(uids))
+
+    # def send(self, email, subject, body):
+    #     """Creates and sends an email message
+    #     """
+    #     lab = api.get_setup().laboratory
+    #     from_addr = lab.getEmailAddress()
+    #     msg = mailapi.compose(from_addr, email, subject, body)
+    #     return mailapi.send_email(mime_msg)
