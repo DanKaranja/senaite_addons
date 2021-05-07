d9614ca86f888da917d544ac6746bfce6fae5e73
Daniel Karanja <Daniel@cohesu.org>
2021-05-07 12:39:27 +0300

    Lims.API Test: 4

--- src/ilara/mods/SMSReport.py
+++ src/ilara/mods/SMSReport.py
@@ -47,19 +47,34 @@ class SMSReport(object):
         value = False
 
         if len(ARReports) > 0:
-            fields = api.get_fields(ARReports[0])
-            SampleTypeTitle = fields.get("SampleTypeTitle")
-            # Patient = fields.get("Patient")
-            value = {"Sample Type": SampleTypeTitle.value}
-        
-        response =  {"message":message,"value":value}
+            try:
+                logger.info("Type of query result: {0}".format(query_results[0].type()))
+            except:
+                logger.info("Getting type of query result failed")
+
+            try:
+                logger.info("Type of object: {0}".format(ARReports[0].type()))
+            except:
+                logger.info("Getting type of object failed")
+
+            try:
+                fields = api.get_fields(ARReports[0])
+                SampleType = fields.get("SampleTypeTitle")
+
+                logger.info("Attempt 1: {0}".format(SampleType))
+            except:
+                logger.info("Attempt 1 Failed")
+
+        # Patient = fields.get("Patient")
+        # value = {"Sample Type": SampleTypeTitle.value}
+        # response =  {"message":message,"value":value}
 
 
         # query = {"portal_type": ["Contact", "LabContact"]}
         # contacts = map(api.get_object, api.search(query, "portal_catalog"))
         # emails = map(lambda c: c.getEmailAddress(), contacts)
         # emails = filter(None, emails)
-        return response
+        return True
 
     # def get_emails(self):
     #     """Returns the emails from all registered contacts
