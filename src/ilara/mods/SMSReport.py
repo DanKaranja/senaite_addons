54357ba5f70c2586140cc685e2540b93d76ab45b
Daniel Karanja <Daniel@cohesu.org>
2021-05-07 13:30:36 +0300

    Lims.API Test: 4

--- src/ilara/mods/SMSReport.py
+++ src/ilara/mods/SMSReport.py
@@ -46,18 +46,18 @@ class SMSReport(object):
         if len(AnalysisRequests) > 0:
             try:
                 logger.info("Attempt 1: {0}".format(query_results[0].review_state))
-            except:
-                logger.info("Attempt 1 failed")
+            except Exception as e:
+                logger.info("Attempt 1 failed: {0}".format(e))
 
             try:
                 logger.info("Attempt 2: {0}".format(query_results[0].Patient.uid))
-            except:
-                logger.info("Attempt 2: failed")
+            except Exception as e:
+                logger.info("Attempt 2: failed: {0}".format(e))
             
             try:
                 logger.info("Attempt 3: {0}".format(query_results[0].Patient))
-            except:
-                logger.info("Attempt 3: failed")
+            except Exception as e:
+                logger.info("Attempt 3: failed: {0}".format(e))
 
         # Patient = fields.get("Patient")
         # value = {"Sample Type": SampleTypeTitle.value}
