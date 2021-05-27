f6d2182a7645e9e4c88467259518a5b27b360626
Daniel Karanja <Daniel@cohesu.org>
2021-05-27 14:28:36 +0300

    attempting to add payment object

--- src/ilara/mods/PushContentTypes.py
+++ src/ilara/mods/PushContentTypes.py
@@ -22,7 +22,7 @@ class IlaraContentTypes(object):
 
         # self.data.get("sample_id")
 
-        item = api.create(api.get_portal().payments, "payments", title="Test Payments")
+        item = api.create(api.get_portal().payments, "payents", title="Test Payments")
 
         return response
     
