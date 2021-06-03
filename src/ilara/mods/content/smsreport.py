46cc09742bd4eaa8744cea4e1b6a400e94060da6
Daniel Karanja <Daniel@cohesu.org>
2021-06-03 23:08:46 +0300

    Importing smsreport content type into  add-on

---
+++ src/ilara/mods/content/smsreport.py
@@ -0,0 +1,35 @@
+from plone.app.textfield import RichText
+from plone.autoform import directives
+from plone.namedfile import field as namedfile
+from plone.supermodel.directives import fieldset
+from plone.supermodel import model
+from z3c.form.browser.radio import RadioFieldWidget
+from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+
+from ploneconf.site import MessageFactory as _
+
+class smsreport(model.Schema):
+    """Dexterity-Schema for SMS reports
+    """
+
+    phone_number = TextLine(
+        title=_(u"phone_number"),
+        required=False
+    )
+
+    sms_message = TextLine(
+        title=_(u"sms_message"),
+        required=False
+    )
+
+    date_published = TextLine(
+        title=_(u"date_published"),
+        required=False
+    )
+
+    report_uid = TextLine(
+        title=_(u"report_uid"),
+        required=False
+    )
