1cde51cd9f416ef812ac99c15bad3290d812e697
Daniel Karanja <Daniel@cohesu.org>
2021-09-26 15:46:40 +0300

    Attempting to get the url

--- src/ilara/mods/IlaraSamplesAdapter.py
+++ src/ilara/mods/IlaraSamplesAdapter.py
@@ -48,7 +48,7 @@ class IlaraSamplesAdapter(object):
 
         sample = api.get_object(obj)
         sample_title = sample.Title()
-        logger.info('ContactÙRL: {0}'.format(sample_title.getContactURL))
+        # logger.info('ContactÙRL: {0}'.format(sample_title.getContactURL))
 
         payment_app_url = base_url+'payments?sampleid='+sample_title
         item['request_payment'] = "<a href='%s' target='_blank'>Not paid</a>" % payment_app_url
